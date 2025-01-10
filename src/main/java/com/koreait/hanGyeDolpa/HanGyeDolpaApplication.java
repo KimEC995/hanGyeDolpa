b4787feea18a9f1f265755713185ee1f3340f230
KimEC995 <kimec995@gmail.com>
2025-01-10 19:23:36 +0900

    Commit Test - Init Project

---
+++ src/main/java/com/koreait/hanGyeDolpa/HanGyeDolpaApplication.java
@@ -0,0 +1,13 @@
+package com.koreait.hanGyeDolpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanGyeDolpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HanGyeDolpaApplication.class, args);
+	}
+
+}
