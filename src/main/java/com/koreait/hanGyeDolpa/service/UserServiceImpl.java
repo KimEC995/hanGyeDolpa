24c95abd4347c1024965ea26899ee22e65a1c7e7
KimEC995 <kimec995@gmail.com>
2025-01-16 20:37:21 +0900

    123

---
+++ src/main/java/com/koreait/hanGyeDolpa/service/UserServiceImpl.java
@@ -0,0 +1,28 @@
+package com.koreait.hanGyeDolpa.service;
+
+import org.springframework.stereotype.Service;
+
+import com.koreait.hanGyeDolpa.bean.UserVO;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+	@Override
+	public UserVO makeSampleUserData() {
+		UserVO vo = new UserVO();
+		vo.setUserID("SampleUser01");
+		vo.setUserName("QWERTY");
+		vo.setUserAlias("Samply");
+		vo.setUserNo(1L);
+		vo.setUserProfilePath("img/userLogoSample.png");
+		return vo;
+	}
+
+	@Override
+	public boolean makeUserLogin(boolean flag, boolean str) {
+		flag = str;
+		
+		return flag;
+	}
+
+}
