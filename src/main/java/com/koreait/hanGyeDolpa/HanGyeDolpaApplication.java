99ac58b044b190e7f83e1e7536e1d20f5f16e768
shinwoongkyu <woongkyushin@gmail.com>
2025-01-20 20:49:42 +0900

    shinwoongkyu

--- src/main/java/com/koreait/hanGyeDolpa/HanGyeDolpaApplication.java
+++ src/main/java/com/koreait/hanGyeDolpa/HanGyeDolpaApplication.java
@@ -2,14 +2,14 @@ package com.koreait.hanGyeDolpa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class HanGyeDolpaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HanGyeDolpaApplication.class, args);
 		
-		dslafjalkfj
 	}
 
 }
