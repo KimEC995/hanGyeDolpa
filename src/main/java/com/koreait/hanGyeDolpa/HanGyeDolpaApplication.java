d788e65f55d95aa0db05e98ef53b9ae76ee78c72
kittyjh19 <kittyjh1019.gmail.com>
2025-01-22 20:24:55 +0900

    2nd

--- src/main/java/com/koreait/hanGyeDolpa/HanGyeDolpaApplication.java
+++ src/main/java/com/koreait/hanGyeDolpa/HanGyeDolpaApplication.java
@@ -2,10 +2,9 @@ package com.koreait.hanGyeDolpa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+@SpringBootApplication
 @EnableJpaRepositories(basePackages = "com.koreait.hanGyeDolpa.repository")
 public class HanGyeDolpaApplication {
 
